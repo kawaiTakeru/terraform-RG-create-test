9a92f4fac7f0147d1eeb38028dcfb28822d801c6
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-23 14:29:52 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-output "message" {
-  value = "Terraform executed successfully."
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-terraform-test-01"
+  location = "japaneast"  # リージョンは必要に応じて変更
 }
