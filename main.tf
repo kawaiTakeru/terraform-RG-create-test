e19f14831e51efde58d386d1b252912b975e5d5e
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-23 14:47:28 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = "6a018b75-55b5-4b68-960d-7328148568aa"
 }
+
 
 resource "azurerm_resource_group" "example" {
   name     = "rg-terraform-test-01"
